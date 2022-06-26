9ff9c6f92a780ac401b16576d14f554f54e17a7f
FellGast <antonshhavelev@gmail.com>
2022-06-26 17:59:38 +0300

    Data Type

---
+++ src/com/max/idea/Main.java
@@ -0,0 +1,14 @@
+package com.max.idea;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double a = 33.3;
+        byte b = 22;
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+}
